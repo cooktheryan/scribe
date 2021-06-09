42cdfecc153706294e01a0ab8e1e155131d32073
Ryan Cook <rcook@redhat.com>
2021-06-09 16:52:16 -0400

    playing
    Signed-off-by: Ryan Cook <rcook@redhat.com>

--- controllers/replicationsourcedefinition_controller.go
+++ controllers/replicationsourcedefinition_controller.go
@@ -21,14 +21,13 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
-	"github.com/operator-framework/operator-lib/status"
-	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	scribev1alpha1 "github.com/backube/scribe/api/v1alpha1"
 )
@@ -41,11 +40,10 @@ type ReplicationSourceDefinitionReconciler struct {
 }
 
 type SourceDefinitionReconciler struct {
-	replicationSource *scribev1alpha1.ReplicationSource
-	Instance          *scribev1alpha1.ReplicationSourceDefinition
-	Ctx               context.Context
-	Log               logr.Logger
-	Scheme            *runtime.Scheme
+	Instance *scribev1alpha1.ReplicationSourceDefinition
+	Ctx      context.Context
+	Log      logr.Logger
+	Scheme   *runtime.Scheme
 	client.Client
 }
 
@@ -58,106 +56,75 @@ type SourceDefinitionReconciler struct {
 
 //nolint:funlen
 func (r *ReplicationSourceDefinitionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	logger := r.Log.WithValues("replicationsourcedefinition", req.NamespacedName)
-	inst := &scribev1alpha1.ReplicationSourceDefinition{}
-	if err := r.Client.Get(ctx, req.NamespacedName, inst); err != nil {
+	log := ctrllog.FromContext(ctx)
+
+	// Fetch the Extract instance
+	instance := &scribev1alpha1.ReplicationSourceDefinition{}
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
 		if kerrors.IsNotFound(err) {
-			logger.Error(err, "Failed to get Definition")
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("Extract resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
 		}
-		return ctrl.Result{}, client.IgnoreNotFound(err)
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get Extract")
+		return ctrl.Result{}, err
 	}
 
-	if inst.Status == nil {
-		inst.Status = &scribev1alpha1.ReplicationSourceDefinitionStatus{}
+	// Check if the RS already exists, if not create a new one
+	found := &scribev1alpha1.ReplicationSource{}
+	err = r.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, found)
+	if err != nil && kerrors.IsNotFound(err) {
+		// Define a new SR
+		SR := r.EnsureSR(instance)
+		log.Info("Creating a new SR", "SR.Namespace", SR.Namespace, "SR.Name", SR.Name)
+		err = r.Create(ctx, SR)
+		if err != nil {
+			log.Error(err, "Failed to create new SR", "SR.Namespace", SR.Namespace, "SR.Name", SR.Name)
+			return ctrl.Result{}, err
+		}
+		// SR created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get SR")
+		return ctrl.Result{}, err
 	}
-	if inst.Status.Conditions == nil {
-		inst.Status.Conditions = status.Conditions{}
-	}
-
-	var result ctrl.Result
-	var err error
-	if inst.Spec.ReplicationMethod != "" {
-		result, err = RunRcloneSrcDefReconciler(ctx, inst, r, logger)
-	} else {
-		return ctrl.Result{}, nil
-	}
-
-	// Set reconcile status condition
-	if err == nil {
-		inst.Status.Conditions.SetCondition(
-			status.Condition{
-				Type:    scribev1alpha1.ConditionReconciled,
-				Status:  corev1.ConditionTrue,
-				Reason:  scribev1alpha1.ReconciledReasonComplete,
-				Message: "Reconcile complete",
-			})
-	} else {
-		inst.Status.Conditions.SetCondition(
-			status.Condition{
-				Type:    scribev1alpha1.ConditionReconciled,
-				Status:  corev1.ConditionFalse,
-				Reason:  scribev1alpha1.ReconciledReasonError,
-				Message: err.Error(),
-			})
-	}
-
-	return result, err
+	return ctrl.Result{}, nil
 }
 
-// RunRcloneSrcReconciler is invoked when ReplicationSource.Spec.Rclone != nil
-func RunRcloneSrcDefReconciler(
-	ctx context.Context,
-	instance *scribev1alpha1.ReplicationSourceDefinition,
-	sr *ReplicationSourceDefinitionReconciler,
-	logger logr.Logger,
-) (ctrl.Result, error) {
-	r := SourceDefinitionReconciler{
-		Instance: instance,
-		Ctx:      ctx,
+//nolint:lll
+//nolint:funlen
+func (r *ReplicationSourceDefinitionReconciler) EnsureSR(m *scribev1alpha1.ReplicationSourceDefinition) *scribev1alpha1.ReplicationSource {
+	SR := &scribev1alpha1.ReplicationSource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: scribev1alpha1.ReplicationSourceSpec{
+			SourcePVC: "mypvc",
+			Trigger:   &scribev1alpha1.ReplicationSourceTriggerSpec{},
+			Rclone: &scribev1alpha1.ReplicationSourceRcloneSpec{
+				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
+					CopyMethod: "None",
+				},
+				RcloneConfigSection: m.Spec.RcloneConfigSection,
+				RcloneDestPath:      m.Spec.RcloneDestPath,
+				RcloneConfig:        m.Spec.RcloneConfig,
+			},
+		},
 	}
-
-	l := logger.WithValues("method", "Rclone")
-
-	_, err := reconcileBatch(l,
-		r.ensureSource,
-	)
-	return ctrl.Result{}, err
+	// Set Memcached instance as the owner and controller
+	ctrl.SetControllerReference(m, SR, r.Scheme)
+	return SR
 }
 
+// SetupWithManager sets up the controller with the Manager.
 func (r *ReplicationSourceDefinitionReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&scribev1alpha1.ReplicationSourceDefinition{}).
 		Owns(&scribev1alpha1.ReplicationSource{}).
 		Complete(r)
-}
-
-//nolint:funlen
-func (r *SourceDefinitionReconciler) ensureSource(l logr.Logger) (bool, error) {
-	r.replicationSource = &scribev1alpha1.ReplicationSource{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "scribe-rclone-src-" + r.Instance.Name,
-			Namespace: r.Instance.Namespace,
-		},
-	}
-	logger := l.WithValues("job", nameFor(r.replicationSource))
-	op, err := ctrlutil.CreateOrUpdate(r.Ctx, r.Client, r.replicationSource, func() error {
-		if err := ctrl.SetControllerReference(r.Instance, r.replicationSource, r.Scheme); err != nil {
-			logger.Error(err, "unable to set controller reference")
-			return err
-		}
-
-		r.replicationSource.Spec.SourcePVC = "mypvc"
-		r.replicationSource.Spec.Rclone.RcloneConfigSection = r.Instance.Spec.RcloneConfigSection
-		r.replicationSource.Spec.Rclone.RcloneDestPath = r.Instance.Spec.RcloneConfigSection
-		r.replicationSource.Spec.Rclone.RcloneConfig = r.Instance.Spec.RcloneConfigSection
-		r.replicationSource.Spec.Rclone.CopyMethod = r.Instance.Spec.CopyMethod
-		logger.V(1).Info("Job has PVC")
-		return nil
-	})
-	if err != nil {
-		logger.Error(err, "reconcile failed")
-	} else {
-		logger.V(1).Info("Definition reconciled", "operation", op)
-	}
-	return true, nil
 }
